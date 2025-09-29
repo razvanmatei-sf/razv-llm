58987f7ab71475ae12410f2c2d8c9642af844714
razv23 <78099724+razv23@users.noreply.github.com>
2025-09-29 11:30:33 +0200

    Revert "Fix dynamic image slots with JavaScript frontend implementation"
    This reverts commit 189dc24840720b555a6ada40220d29b699afc6e0.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,3 @@
 from .llm_chat import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-# Declare web directory for JavaScript files
-WEB_DIRECTORY = "./web"
-
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
